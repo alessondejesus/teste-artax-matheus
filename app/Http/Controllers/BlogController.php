e05d272a997b6a54a24df2576ae6cc62ef03f61f
matheuskoba <matheuskoba2021@icloud.com>
2022-06-06 08:47:04 -0300

    test

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,7 +11,7 @@ use App\Http\Requests\UpdateBlogRequest;
 class BlogController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource
      *
      * @return \Illuminate\Http\Response
      */
